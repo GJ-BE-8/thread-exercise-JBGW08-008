d990c74592cc662b850d4b2e1b3182ed489c469f
{minkyuSnow} <{bingseol99@gmail.com}>
2024-09-12 23:28:02 +0900

    실습 1-5 commit

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -16,19 +16,21 @@ import com.nhnacademy.thread.CounterHandler;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class App
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         CounterHandler counterHandler = new CounterHandler(10l);
         Thread thread = new Thread(counterHandler);
-        log.debug("thread-state:{}",thread.getState());
+        log.debug("thread-state:{}", thread.getState());
         thread.setName("my-counter");
         thread.start();
         //TODO#1 thread가 실행 후 (1-10 count 증가 후  아래 로그가 출력 됩니다.)
         //thread.join()을 호출 하면 thread가 종료될 때 까지 main thread가 대기하게 됩니다.
-
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         log.debug("Application exit!");
-        log.debug("thread-state:{}",thread.getState());
+        log.debug("thread-state:{}", thread.getState());
     }
 }
