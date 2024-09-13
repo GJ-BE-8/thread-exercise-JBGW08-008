7671fcc07ffad5bc28af071003eeadc785ddb28b
{minkyuSnow} <{bingseol99@gmail.com}>
2024-09-13 09:26:42 +0900

    실습 2-1 commit

--- src/main/java/com/nhnacademy/count/SharedCounter.java
+++ src/main/java/com/nhnacademy/count/SharedCounter.java
@@ -15,32 +15,34 @@ package com.nhnacademy.count;
 public class SharedCounter {
     private long count;
 
-    public SharedCounter(){
-        count =0l;
+    public SharedCounter() {
+        count = 0l;
     }
 
     public SharedCounter(long count) {
         //TODO#1-1 생성자를 초기화 합니다. count < 0 IllegalArgumentException아 발생 합니다.
-
+        if (count < 0) {
+            throw new IllegalArgumentException("count < 0");
+        }
         this.count = 0l;
 
     }
 
-    public long getCount(){
+    public long getCount() {
         //TODO#1-2 count 를 반환 합니다.
 
-        return 0l;
+        return this.count;
     }
 
-    public long increaseAndGet(){
+    public long increaseAndGet() {
         //TODO#1-3 count = count + 1 증가시키고 count를 반환 합니다.
-
-        return 0l;
+        this.count = this.count + 1;
+        return this.count;
     }
 
-    public long decreaseAndGet(){
+    public long decreaseAndGet() {
         //TODO#1-4 count = count-1 감소시키고 count를 반환 합니다.
-
-        return 0l;
+        this.count = this.count - 1;
+        return this.count;
     }
 }
